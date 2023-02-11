9b1b36af5900446171da6021d022ca1eecf9f843
AngelUche <ucheorji9974@gmail.com>
2023-02-11 18:24:45 +0000

    added navbar

--- src/components/navigation/MobileNav.tsx
+++ src/components/navigation/MobileNav.tsx
@@ -1,9 +1,35 @@
 import "./MobileNav.css";
 import NavLinks from "./NavLinks";
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+
 function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  const OpenIcon = (
+    <AiOutlineMenu
+      className="MobileNav__icon"
+      size="30px"
+      color="color"
+      onClick={toggle}
+    />
+  );
+  const CloseIcon = (
+    <AiOutlineClose
+      className="MobileNav__icon"
+      size="30px"
+      color="color"
+      onClick={toggle}
+    />
+  );
   return (
     <div className="MobileNav">
-      <NavLinks />
+      {isOpen ? CloseIcon : OpenIcon}
+      {isOpen && <NavLinks />}
     </div>
   );
 }
