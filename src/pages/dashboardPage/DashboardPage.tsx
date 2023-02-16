19ab277e726625c9cc4abfe89c054305230d42f5
mightGuy <onyejemeemmanuel65@gmail.com>
2023-02-16 07:04:58 +0100

    Added dashboard footer links icons

--- src/pages/dashboardPage/DashboardPage.tsx
+++ src/pages/dashboardPage/DashboardPage.tsx
@@ -6,11 +6,18 @@ import { FooterNav } from "./FooterNav";
 function DashboardPage() {
   return (
     <div className="dash-container">
+      
+      {/* Desktop side navigation */}
       <div className="dash-aside">
         <h1>Desktop Side nav</h1>
       </div>
+
+      {/* Main Page */}
       <div className="dash-main">
+        {/* Render default home component */}
         <Outlet />
+
+        {/* Mobile Footer Menu */}
         <FooterNav />
       </div>
     </div>
